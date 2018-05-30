25c4498059748d0017ab50116d7dbead649e6b65
Thiago Almeida <taalmeid@hotmail.com>
2018-05-30 17:32:20 -0300

    First Commit

---
+++ HotelBot/Dialogs/HotelBotDialog.cs
@@ -0,0 +1,34 @@
+﻿using HotelBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HotelBot.Dialogs
+{
+    public class HotelBotDialog
+    {
+        public static readonly IDialog<string> dialog = Chain.PostToChain()
+            .Select(msg => msg.Text)
+            .Switch(
+            new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase), (context, text) =>
+              {
+                  return Chain.ContinueWith(new GreetingDialog(), AfterGreetingContinuation);
+              }),
+            new DefaultCase<string, IDialog<string>>((context, text) =>
+            {
+                return Chain.ContinueWith(FormDialog.FromForm(RoomReservation.BuildForm, FormOptions.PromptInStart), AfterGreetingContinuation);
+            }))
+            .Unwrap()
+            .PostToUser();
+        private async static Task<IDialog<string>> AfterGreetingContinuation(IBotContext context, IAwaitable<object> item)
+        {
+            var token = await item;
+            var name = "User";
+            context.UserData.TryGetValue<string>("Name", out name);
+            return Chain.Return($"Thanks For Using Hotel Bot {name}");
+
+        }
+    }
+}
---
+++ HotelBot/Models/RoomReservation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelBot.Models
+{
+    public enum BedSizeOptions
+    {
+        King,
+        Queen,
+        Single,
+        Double
+    }
+
+    public enum AmenitiesOptions
+    {
+        Kitchen,
+        ExtraTowels,
+        GymAccess,
+        Wifi
+    }
+
+    [Serializable]
+    public class RoomReservation
+    {
+        public BedSizeOptions? BedSize;
+        public int? NumberOfOccupants;
+        public DateTime? CheckInDate;
+        public int? NumberOfDaysToStay;
+        public List<AmenitiesOptions> Amenities;
+
+        public static IForm<RoomReservation> BuildForm()
+        {
+            return new FormBuilder<RoomReservation>()
+                .Message("Welcome To The Jungle Hotel!")
+                .Build();
+        }
+    }
+}
